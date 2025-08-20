3f64e1955601bc023759026f82fe7a2dc53d281d
Anthony <thonyrivera9@gmail.com>
2025-08-20 16:32:58 -0500

    cambio en vite.config

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,14 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  worker: {
+    format: 'es', // ⚡ Usa módulos ES en workers (evita el error de IIFE)
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: undefined, // ⚡ evita conflictos de code splitting en workers
+      },
+    },
+  },
 })
