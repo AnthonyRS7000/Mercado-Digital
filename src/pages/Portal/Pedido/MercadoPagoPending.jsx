2160d9b8e17f6a60fcb1b5756aa081e978c201a2
Anthony <thonyrivera9@gmail.com>
2025-08-25 10:49:20 -0500

    cambios en vista de pagos rechazado y pendiente

--- src/pages/Portal/Pedido/MercadoPagoPending.jsx
+++ src/pages/Portal/Pedido/MercadoPagoPending.jsx
@@ -1,15 +1,27 @@
 import { useNavigate } from "react-router-dom";
+import styles from "./css/MercadoPagoPending.module.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHourglassHalf, faArrowLeft } from "@fortawesome/free-solid-svg-icons";
 
 const MercadoPagoPending = () => {
   const navigate = useNavigate();
 
   return (
-    <div className="text-center mt-5">
-      <h2 className="text-warning">⏳ Tu pago está pendiente</h2>
-      <p>Estamos esperando la confirmación de tu pago.</p>
-      <button className="btn btn-secondary mt-3" onClick={() => navigate("/")}>
-        Volver al inicio
-      </button>
+    <div className={styles.container}>
+      <div className={styles.card}>
+        <FontAwesomeIcon icon={faHourglassHalf} className={styles.icon} />
+        <h2 className={styles.title}>Tu pago está pendiente</h2>
+        <p className={styles.message}>
+          Estamos esperando la confirmación de tu pago. <br />
+          Te notificaremos cuando el proceso finalice.
+        </p>
+
+        <div className={styles.actions}>
+          <button className={styles.homeButton} onClick={() => navigate("/")}>
+            <FontAwesomeIcon icon={faArrowLeft} /> Volver al inicio
+          </button>
+        </div>
+      </div>
     </div>
   );
 };
